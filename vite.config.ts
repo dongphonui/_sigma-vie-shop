82282aaf5bb712c9834d3ea98fa1598d3390115c
dongphonui <65768876+dongphonui@users.noreply.github.com>
2026-01-02 19:06:13 +0700

    Sua loi deploy 001

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, loadEnv } from 'vite'
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -14,6 +15,8 @@ export default defineConfig({
     sourcemap: false
   },
   define: {
-    'process.env': process.env
+    'process.env': {
+      API_KEY: JSON.stringify(process.env.API_KEY || '')
+    }
   }
 })
